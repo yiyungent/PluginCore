72db653cd44ee54650a01ea3b088b62c08bc3e97
yiyun <yiyungent@gmail.com>
2021-08-08 20:34:20 +0800

    feat(fronted-admin/src/views/login/index.vue): pretty

--- fronted-admin/src/views/login/index.vue
+++ fronted-admin/src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">PluginCore Admin</h3>
       </div>
 
       <el-form-item prop="username">
@@ -44,8 +44,8 @@
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
+        <span style="margin-right:20px;">初始用户名: admin</span>
+        <span>初始密码: ABC12345</span>
       </div>
 
     </el-form>
