4459fbe5e2cbe369519b7010a7b7d6d4600738cf
yiyun <yiyungent@gmail.com>
2021-08-16 18:22:14 +0800

    feat(plugincore.iplugins): iStartupPlugin.cs, PluginCore.IPlugins.csproj

---
+++ src/PluginCore.IPlugins/Interfaces/IStartupPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PluginCore.IPlugins
+{
+    public interface IStartupPlugin : IPlugin
+    {
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        void ConfigureServices(IServiceCollection services);
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        void Configure(IApplicationBuilder app);
+    }
+}
