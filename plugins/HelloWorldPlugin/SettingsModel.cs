6d2a518765f7795febded5f4ea5ead57d7b7e417
yiyun <yiyungent@gmail.com>
2021-08-19 18:57:48 +0800

    feat(plugins/helloworldplugin): remove GitHub API

--- plugins/HelloWorldPlugin/SettingsModel.cs
+++ plugins/HelloWorldPlugin/SettingsModel.cs
@@ -9,26 +9,5 @@ namespace HelloWorldPlugin
     public class SettingsModel : PluginSettingsModel
     {
         public string Hello { get; set; }
-
-        public GitHubOAuthModel GitHubOAuth { get; set; }
-
-        public sealed class GitHubOAuthModel
-        {
-            public string AppName { get; set; }
-
-            public string ClientId { get; set; }
-
-            public string ClientSecret { get; set; }
-
-            public string RedirectUri { get; set; }
-
-            public IList<string> Scopes { get; set; }
-
-            public GitHubOAuthModel()
-            {
-                this.Scopes = new string[] { "user", "notifications" };
-                this.RedirectUri = "/api/GitHub/Authorize";
-            }
-        }
     }
 }
