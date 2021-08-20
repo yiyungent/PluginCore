d4519b54e9df931c6e75d9ca59742edc5f3185ac
yiyun <yiyungent@gmail.com>
2021-08-20 12:05:53 +0800

    feat(istartupplugin.cs,istartupxplugin.cs): 添加注释

--- src/PluginCore.IPlugins/Interfaces/IStartupPlugin.cs
+++ src/PluginCore.IPlugins/Interfaces/IStartupPlugin.cs
@@ -4,12 +4,14 @@ using Microsoft.Extensions.DependencyInjection;
 namespace PluginCore.IPlugins
 {
     /// <summary>
-    /// 目前未有效化，占坑
+    /// 实验阶段
+    /// <para>无法热插拔: 需要启用插件后, 再 重启站点</para>
     /// </summary>
     public interface IStartupPlugin : IPlugin
     {
         /// <summary>
         /// This method gets called by the runtime. Use this method to add services to the container.
+        /// <para>未有效化</para>
         /// </summary>
         /// <param name="services"></param>
         void ConfigureServices(IServiceCollection services);
--- src/PluginCore.IPlugins/Interfaces/IStartupXPlugin.cs
+++ src/PluginCore.IPlugins/Interfaces/IStartupXPlugin.cs
@@ -6,10 +6,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace PluginCore.IPlugins
 {
+    /// <summary>
+    /// 实验阶段
+    /// <para>热插拔: 已有效化</para>
+    /// </summary>
     public interface IStartupXPlugin : IPlugin
     {
         /// <summary>
         /// This method gets called by the runtime. Use this method to add services to the container.
+        /// <para>未有效化</para>
         /// </summary>
         /// <param name="services"></param>
         void ConfigureServices(IServiceCollection services);
