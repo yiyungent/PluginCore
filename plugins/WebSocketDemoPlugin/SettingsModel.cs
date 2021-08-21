0257e94a35d84b11cbce41f65d5d67157e871880
yiyun <yiyungent@gmail.com>
2021-08-21 10:23:39 +0800

    feat(plugins/websocketdemoplugin): new empty plugin project

---
+++ plugins/WebSocketDemoPlugin/SettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using PluginCore.Models;
+
+namespace WebSocketDemoPlugin
+{
+    public class SettingsModel : PluginSettingsModel
+    {
+        public string Hello { get; set; }
+    }
+}
---
+++ plugins/WebSocketDemoPlugin/WebSocketDemoPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PluginCore.IPlugins;
+
+namespace WebSocketDemoPlugin
+{
+    public class WebSocketDemoPlugin : BasePlugin, IStartupXPlugin
+    {
+        public override (bool IsSuccess, string Message) AfterEnable()
+        {
+            Console.WriteLine($"{nameof(WebSocketDemoPlugin)}: {nameof(AfterEnable)}");
+            return base.AfterEnable();
+        }
+
+        public override (bool IsSuccess, string Message) BeforeDisable()
+        {
+            Console.WriteLine($"{nameof(WebSocketDemoPlugin)}: {nameof(BeforeDisable)}");
+            return base.BeforeDisable();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+        }
+    }
+}
