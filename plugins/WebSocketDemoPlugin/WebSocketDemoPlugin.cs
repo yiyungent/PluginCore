240f6a8c2a44f09cdd5d8b9e7c5392748b18f54b
yiyun <yiyungent@gmail.com>
2021-08-22 22:29:29 +0800

    feat(plugins): webSocketDemoPlugin: 测试成功

--- plugins/WebSocketDemoPlugin/WebSocketDemoPlugin.cs
+++ plugins/WebSocketDemoPlugin/WebSocketDemoPlugin.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using PluginCore.IPlugins;
 
 namespace WebSocketDemoPlugin
 {
-    public class WebSocketDemoPlugin : BasePlugin, IStartupXPlugin
+    public class WebSocketDemoPlugin : BasePlugin, /*IStartupXPlugin,*/ IStartupPlugin
     {
         public override (bool IsSuccess, string Message) AfterEnable()
         {
@@ -26,8 +31,55 @@ namespace WebSocketDemoPlugin
 
         public void Configure(IApplicationBuilder app)
         {
+            // 1. 首先必须 添加 WebSocketMiddleware
+            var webSocketOptions = new WebSocketOptions()
+            {
+                // KeepAliveInterval - 向客户端发送 "ping" 帧的频率，以确保代理保持连接处于打开状态。 默认值为 2 分钟。
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+            };
+            app.UseWebSockets(webSocketOptions);
 
+
+            #region 接受 WebSocket 请求: 方式2: 通过 Middleware
+            //app.Use(async (context, next) =>
+            //    {
+            //        if (context.Request.Path == "/plugins/ws")
+            //        {
+            //            if (context.WebSockets.IsWebSocketRequest)
+            //            {
+            //                using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+            //                {
+            //                    await Echo(context, webSocket);
+            //                }
+            //            }
+            //            else
+            //            {
+            //                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            //            }
+            //        }
+            //        else
+            //        {
+            //            await next();
+            //        }
+
+            //    }); 
+            #endregion
         }
+
+        #region 方式2: 通过 Middleware
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+        #endregion
 
         public int ConfigureOrder
         {
---
+++ plugins/WebSocketDemoPlugin/wwwroot/js/main.js
@@ -0,0 +1,111 @@
+﻿var connectionUrl = document.getElementById("connectionUrl");
+var connectButton = document.getElementById("connectButton");
+var stateLabel = document.getElementById("stateLabel");
+var sendMessage = document.getElementById("sendMessage");
+var sendButton = document.getElementById("sendButton");
+var commsLog = document.getElementById("commsLog");
+var closeButton = document.getElementById("closeButton");
+var socket;
+
+// 注意: 若当前是 https, 则采用 wss, 否则采用 ws
+var scheme = document.location.protocol === "https:" ? "wss" : "ws";
+var port = document.location.port ? (":" + document.location.port) : "";
+
+connectionUrl.value = scheme + "://" + document.location.hostname + port + "/plugins/ws";
+
+function updateState() {
+    function disable() {
+        sendMessage.disabled = true;
+        sendButton.disabled = true;
+        closeButton.disabled = true;
+    }
+    function enable() {
+        sendMessage.disabled = false;
+        sendButton.disabled = false;
+        closeButton.disabled = false;
+    }
+
+    connectionUrl.disabled = true;
+    connectButton.disabled = true;
+
+    if (!socket) {
+        disable();
+    } else {
+        switch (socket.readyState) {
+            case WebSocket.CLOSED:
+                stateLabel.innerHTML = "Closed";
+                disable();
+                connectionUrl.disabled = false;
+                connectButton.disabled = false;
+                break;
+            case WebSocket.CLOSING:
+                stateLabel.innerHTML = "Closing...";
+                disable();
+                break;
+            case WebSocket.CONNECTING:
+                stateLabel.innerHTML = "Connecting...";
+                disable();
+                break;
+            case WebSocket.OPEN:
+                stateLabel.innerHTML = "Open";
+                enable();
+                break;
+            default:
+                stateLabel.innerHTML = "Unknown WebSocket State: " + htmlEscape(socket.readyState);
+                disable();
+                break;
+        }
+    }
+}
+
+closeButton.onclick = function () {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert("socket not connected");
+    }
+    socket.close(1000, "Closing from client");
+};
+
+sendButton.onclick = function () {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert("socket not connected");
+    }
+    var data = sendMessage.value;
+    socket.send(data);
+    commsLog.innerHTML += '<tr>' +
+        '<td class="commslog-client">Client</td>' +
+        '<td class="commslog-server">Server</td>' +
+        '<td class="commslog-data">' + htmlEscape(data) + '</td></tr>';
+};
+
+connectButton.onclick = function () {
+    stateLabel.innerHTML = "Connecting";
+    socket = new WebSocket(connectionUrl.value);
+    socket.onopen = function (event) {
+        updateState();
+        commsLog.innerHTML += '<tr>' +
+            '<td colspan="3" class="commslog-data">Connection opened</td>' +
+            '</tr>';
+    };
+    socket.onclose = function (event) {
+        updateState();
+        commsLog.innerHTML += '<tr>' +
+            '<td colspan="3" class="commslog-data">Connection closed. Code: ' + htmlEscape(event.code) + '. Reason: ' + htmlEscape(event.reason) + '</td>' +
+            '</tr>';
+    };
+    socket.onerror = updateState;
+    socket.onmessage = function (event) {
+        commsLog.innerHTML += '<tr>' +
+            '<td class="commslog-server">Server</td>' +
+            '<td class="commslog-client">Client</td>' +
+            '<td class="commslog-data">' + htmlEscape(event.data) + '</td></tr>';
+    };
+};
+
+function htmlEscape(str) {
+    return str.toString()
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
