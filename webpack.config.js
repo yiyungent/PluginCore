c5ee197a3f8f83db6f905bc54c334caf7c775609
yiyun <yiyungent@gmail.com>
2022-02-19 21:57:06 +0800

    feat(.babelrc,package-lock.json,package.json,webpack.config.js): add: babel 转换

--- webpack.config.js
+++ webpack.config.js
@@ -33,9 +33,15 @@ module.exports = {
         ],
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
