db88ff66f5a7d81958d0bcdecf2d9dcd4dda328c
yiyun <yiyungent@gmail.com>
2022-02-20 15:47:55 +0800

    feat(hexoplugin): add

---
+++ plugins/HexoPlugin/SettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using PluginCore.Models;
+
+namespace HexoPlugin
+{
+    public class SettingsModel : PluginSettingsModel
+    {
+        public string[] Origins { get; set; }
+
+        public string Head { get; set; }
+
+        public string Footer { get; set; }
+
+    }
+}
