8c6498e5a77b51c0696ab19930a1151e38f75755
yiyun <yiyungent@gmail.com>
2022-04-13 03:43:05 +0800

    fix(src/plugincore/pluginmanager.cs): 尝试允许 插件依赖的第三包 与 主程序 相同包 不同版本

--- src/PluginCore/PluginManager.cs
+++ src/PluginCore/PluginManager.cs
@@ -17,30 +17,43 @@ namespace PluginCore
     {
         private readonly PluginControllerManager _pluginControllerManager;
 
-        public IList<string> SkipDlls { get; set; }
+        public IList<string> SkipDllKeys { get; set; }
 
         public PluginManager(PluginControllerManager pluginControllerManager)
         {
             _pluginControllerManager = pluginControllerManager;
-            SkipDlls = new List<string>();
+            SkipDllKeys = new List<string>();
 
             #region 跳过1: 程序目录下 以单独dll 出现
             // 获取主程序 已经存在的（不许在再加载的 dll）
             string basePath = AppContext.BaseDirectory;
             //Console.WriteLine($"PluginCore.PluginManager: basePath: {basePath}");
             // { "Core.dll", "Domain.dll", "Framework.dll", "Services.dll", "Repositories.dll", "PluginCore.dll", ... }
-            SkipDlls = new DirectoryInfo(basePath).GetFiles("*.dll").Select(m => m.Name).ToList();
+
+            // 为保证万无一失
+            SkipDllKeys = DllKeysInDir(new DirectoryInfo(basePath)).Select(m => m.DllKey).ToList();
             #endregion
 
             #region 跳过2: 打包进入1个dll 或 打包进 1个exe
             // 注意: 用户可能将 dll 打包在 一个dll中, 或打包进 exe, 因此 通过此方式 确保跳过
             // 主程序所有 位于 AssemblyLoadContext.Default
-            List<string> skipAssembliesName = AssemblyLoadContext.Default.Assemblies
+            // 注意: 这样获取到的 会漏一些，不知道为什么, 可能将其算在了 assembly.GetReferencedAssemblies() 里
+            List<AssemblyName> skipAssembliesName = AssemblyLoadContext.Default.Assemblies
                 .Select(m => m.GetName())
-                .Select(m => m.Name).ToList();
+                //.Select(m => m.Name).ToList();
+                .ToList();
+            //foreach (var name in skipAssembliesName)
             foreach (var name in skipAssembliesName)
             {
-                this.SkipDlls.Add($"{name}.dll");
+                // 只区分 名、版本
+                // driver, Version=1.5.77
+                //this.SkipDlls.Add($"{name.Name}, Version={name.Version}");
+
+                // 全名
+                if (!SkipDllKeys.Contains(name.FullName))
+                {
+                    this.SkipDllKeys.Add(name.FullName);
+                }
             }
             #endregion
         }
@@ -62,7 +75,7 @@ namespace PluginCore
             //}
 
             // 跳过不需要加载的 dll, eg: ASP.NET Core Shared Framework, 主程序中已有dll
-            string[] skipDlls = SkipDlls.ToArray(); //new string[] { "Core.dll", "Domain.dll", "Framework.dll", "Services.dll", "Repositories.dll", "PluginCore.dll" };
+            string[] skipDllKeys = SkipDllKeys.ToArray(); //new string[] { "Core.dll", "Domain.dll", "Framework.dll", "Services.dll", "Repositories.dll", "PluginCore.dll" };
 
             // 注意: 先加载插件引用的dll, 因为可能在插件主dll的Controller中立即使用了引用的dll
 
@@ -76,10 +89,17 @@ namespace PluginCore
             // 这里简单来说，意思就是当在一个自定义LoadContext中加载程序集的时候，如果找不到这个程序集，程序会自动去默认LoadContext中查找，如果默认LoadContext中都找不到，就会返回null。
             // 这里我突然想到会不会是因为DemoPlugin1、DemoPlugin2以及主站点的AssemblyLoadContext都加载了Mystique.Core.dll程序集的缘故，虽然他们加载的是同一个程序集，但是因为LoadContext不同，所以系统认为它是2个程序集。
             // 参考： https://www.cnblogs.com/lwqlun/p/12930713.html
-            var allReferenceFileInfos = pluginDir.GetFiles("*.dll").Where(p =>
-            p.Name != $"{pluginId}.dll"
-            &&
-            !skipDlls.Contains(p.Name));
+
+            var allReferenceFileInfos = DllKeysInDir(pluginDir).Where(m =>
+                  m.DllFile.Name != $"{pluginId}.dll"
+                  &&
+                  (
+                      !skipDllKeys.Contains(m.DllKey))
+                      //||
+                      //!m.DllKey.StartsWith("PluginCore")
+                  )
+                  .Select(m => m.DllFile).ToList();
+
             foreach (FileInfo file in allReferenceFileInfos)
             {
                 using (var sr = new StreamReader(file.OpenRead()))
@@ -114,6 +134,32 @@ namespace PluginCore
 
             // 移除其中的控制器
             _pluginControllerManager.RemoveControllers(pluginId);
+        }
+
+        /// <summary>
+        /// 某个目录下的所有 dll 的信息标记
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<(FileInfo DllFile, string DllKey)> DllKeysInDir(DirectoryInfo dllDir)
+        {
+            var alldllFileInfos = dllDir.GetFiles("*.dll");
+
+            // 注意: 专门用一个临时 ALC 来获取当前插件下的 dll 信息
+            var contextTemp = new CollectibleAssemblyLoadContext();
+            foreach (FileInfo file in alldllFileInfos)
+            {
+                using (var sr = new StreamReader(file.OpenRead()))
+                {
+                    Assembly assembly = contextTemp.LoadFromStream(sr.BaseStream);
+                    //string dllKey = $"{assembly.GetName().Name}, Version={assembly.GetName().Version}";
+                    // 直接用全名
+                    string dllKey = assembly.FullName;
+
+                    yield return (file, dllKey);
+                }
+            }
+
+            contextTemp.Unload();
         }
     }
 }
