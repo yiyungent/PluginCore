bca0a4b9abaa7e92f89481226d30b0d2711c2d38
yiyun <yiyungent@gmail.com>
2023-01-05 15:37:54 +0800

    chore(utils/): copyright -> banquan
    防止误识别为开源协议

--- utils/copyright-batch.bat
+++
@@ -1,8 +0,0 @@
-@REM 注意: 使用时, 请将本文件与 copyright-template.txt 文件放到项目根目录 再执行
-
-for /r %%F in (*.cs) DO (
-    move "%%F" tmp.txt
-    type copyright-template.txt > "%%F"
-    type tmp.txt >> "%%F"
-    del tmp.txt
-)
