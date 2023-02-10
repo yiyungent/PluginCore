196823d6e3b39eb70d1d4bb55d7d4c8433ee64fc
yiyun <yiyungent@gmail.com>
2023-02-10 19:27:44 +0800

    feat(src/plugincore.aspnetcore/controllers/debugcontroller.cs): add
    1.PluginContexts 2.AssemblyLoadContexts 3.Assemblies 4.Services

--- src/PluginCore.AspNetCore/Controllers/DebugController.cs
+++ src/PluginCore.AspNetCore/Controllers/DebugController.cs
@@ -7,7 +7,7 @@
 
 
 
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using PluginCore.AspNetCore.Authorization;
 using PluginCore.AspNetCore.ResponseModel;
 using PluginCore.Interfaces;
@@ -16,9 +16,24 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Runtime.Loader;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
 
 namespace PluginCore.AspNetCore.Controllers
 {
+    /// <summary>
+    /// [ASP.NET Core — 依赖注入\_啊晚的博客-CSDN博客\_asp.net core 依赖注入](https://blog.csdn.net/weixin_37648525/article/details/127942292)
+    /// [ASP.NET Core中的依赖注入（3）: 服务的注册与提供 - Artech - 博客园](https://www.cnblogs.com/artech/p/asp-net-core-di-register.html)
+    /// [ASP.NET Core中的依赖注入（5）: ServiceProvider实现揭秘 【总体设计 】 - Artech - 博客园](https://www.cnblogs.com/artech/p/asp-net-core-di-service-provider-1.html)
+    /// [dotnet/ServiceProvider.cs at main · dotnet/dotnet](https://github.com/dotnet/dotnet/blob/main/src/runtime/src/libraries/Microsoft.Extensions.DependencyInjection/src/ServiceProvider.cs)
+    /// [Net6 DI源码分析Part2 Engine,ServiceProvider - 一身大膘 - 博客园](https://www.cnblogs.com/hts92/p/15800990.html)
+    /// [【特别的骚气】asp.net core运行时注入服务，实现类库热插拔 - 四处观察 - 博客园](https://www.cnblogs.com/1996-Chinese-Chen/p/16154218.html)
+    /// 
+    /// ActivatorUtilities.CreateInstance<PluginCore.IPlugins.IPlugin>(serviceProvider, "test");
+    /// ActivatorUtilities.GetServiceOrCreateInstance<PluginCore.IPlugins.IPlugin>(serviceProvider);
+    /// 
+    /// </summary>
     [Route("api/plugincore/admin/[controller]/[action]")]
     [PluginCoreAdminAuthorize]
     [ApiController]
@@ -38,7 +53,7 @@ namespace PluginCore.AspNetCore.Controllers
         #region Actions
 
         [HttpGet, HttpPost]
-        public async Task<ActionResult<BaseResponseModel>> PluginContext()
+        public async Task<ActionResult<BaseResponseModel>> PluginContexts()
         {
             BaseResponseModel responseModel = new BaseResponseModel();
             try
@@ -64,8 +79,131 @@ namespace PluginCore.AspNetCore.Controllers
             return await Task.FromResult(responseModel);
         }
 
+        [HttpGet, HttpPost]
+        public async Task<ActionResult<BaseResponseModel>> AssemblyLoadContexts()
+        {
+            BaseResponseModel responseModel = new BaseResponseModel();
+            try
+            {
+                var assemblyLoadContextDefault = AssemblyLoadContext.Default;
+                var assemblyLoadContextAll = AssemblyLoadContext.All;
+                var responseDataModel = new AssemblyLoadContextsResponseDataModel();
+                responseDataModel.Default = new AssemblyLoadContextsResponseDataModel.AssemblyLoadContextModel
+                {
+                    Name = assemblyLoadContextDefault.Name,
+                    Assemblies = assemblyLoadContextDefault.Assemblies.Select(m => (FullName: m.FullName, DefinedTypes: m.DefinedTypes.Select(m => m.FullName).ToList())).ToList()
+                };
+                responseDataModel.All = assemblyLoadContextAll.Select(item => new AssemblyLoadContextsResponseDataModel.AssemblyLoadContextModel
+                {
+                    Name = item.Name,
+                    Assemblies = item.Assemblies.Select(m => (FullName: m.FullName, DefinedTypes: m.DefinedTypes.Select(m => m.FullName).ToList())).ToList()
+                }).ToList();
+
+                responseModel.Code = 1;
+                responseModel.Message = "success";
+                responseModel.Data = responseDataModel;
+            }
+            catch (Exception ex)
+            {
+                responseModel.Code = -1;
+                responseModel.Message = "error";
+                responseModel.Data = ex.ToString();
+            }
+
+            return await Task.FromResult(responseModel);
+        }
+
+        [HttpGet, HttpPost]
+        public async Task<ActionResult<BaseResponseModel>> Assemblies()
+        {
+            BaseResponseModel responseModel = new BaseResponseModel();
+            try
+            {
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                List<(string FullName, List<string> DefinedTypes)> keyValuePairs = new List<(string, List<string>)>();
+                foreach (var item in assemblies)
+                {
+                    keyValuePairs.Add((item.FullName, item.DefinedTypes.Select(m => m.FullName).ToList()));
+                }
+
+                responseModel.Code = 1;
+                responseModel.Message = "success";
+                responseModel.Data = keyValuePairs;
+            }
+            catch (Exception ex)
+            {
+                responseModel.Code = -1;
+                responseModel.Message = "error";
+                responseModel.Data = ex.ToString();
+            }
+
+            return await Task.FromResult(responseModel);
+        }
+
+        [HttpGet, HttpPost]
+        public async Task<ActionResult<BaseResponseModel>> Services([FromServices] IServiceProvider serviceProvider)
+        {
+            BaseResponseModel responseModel = new BaseResponseModel();
+            try
+            {
+                //IServiceProvider serviceProvider = HttpContext.RequestServices;
+                var provider = serviceProvider.GetType().GetProperty("RootProvider", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                var serviceField = provider.GetType().GetField("_realizedServices", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                var serviceValue = serviceField.GetValue(provider);
+                //var funcType = serviceField.FieldType.GetGenericArguments()[1].GetGenericArguments()[0];
+                //ConcurrentDictionary<Type, Func<ServiceProviderEngineScope, object?>> realizedServices = (ConcurrentDictionary<Type, Func<ServiceProviderEngineScope, object?>>)serviceValue;
+                
+                // TODO: 获取所有已经注册的服务
+                
+
+                List<(string FullName, List<string> DefinedTypes)> keyValuePairs = new List<(string, List<string>)>();
+
+
+                responseModel.Code = 1;
+                responseModel.Message = "success";
+                responseModel.Data = keyValuePairs;
+            }
+            catch (Exception ex)
+            {
+                responseModel.Code = -1;
+                responseModel.Message = "error";
+                responseModel.Data = ex.ToString();
+            }
+
+            return await Task.FromResult(responseModel);
+        }
+
         #endregion
 
-    }
+        public sealed class AssemblyLoadContextsResponseDataModel
+        {
+            public AssemblyLoadContextModel Default
+            {
+                get; set;
+            }
 
+            public List<AssemblyLoadContextModel> All
+            {
+                get; set;
+            }
+
+            public sealed class AssemblyLoadContextModel
+            {
+                public string Name
+                {
+                    get; set;
+                }
+                public List<(string FullName, List<string> DefinedTypes)> Assemblies
+                {
+                    get; set;
+                }
+            }
+        }
+
+        public sealed class AssembliesResponseDataModel
+        {
+
+        }
+
+    }
 }
