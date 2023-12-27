1278d0f4acaa201869e0eb014156e14c6575cd00
yiyun <yiyungent@gmail.com>
2023-12-27 16:19:16 +0800

    feat(src/plugincore.iplugins/constants.cs): add

---
+++ src/PluginCore.IPlugins/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PluginCore.IPlugins
+{
+    public class Constants
+    {
+        public const string AspNetCoreAuthenticationClaimType = "PluginCore.Token";
+
+        public const string AspNetCoreAuthorizationPolicyName = "PluginCore.Admin";
+
+        public const string AspNetCoreAuthorizationTokenCookieName = "PluginCore.Admin.Token";
+    }
+}
