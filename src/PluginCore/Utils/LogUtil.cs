d638a76fbc5733ec19c7dc24450e18b1c5803f70
yiyun <yiyungent@gmail.com>
2023-12-27 17:33:52 +0800

    feat(src/plugincore): utils/LogUtil.cs, PluginCore.csproj: 与 ILogger 结合, FrameworkReference

--- src/PluginCore/Utils/LogUtil.cs
+++ src/PluginCore/Utils/LogUtil.cs
@@ -7,53 +7,80 @@
 
 
 
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PluginCore.Utils
 {
     public class LogUtil
     {
-        public const string Sign = "PluginCore";
+        private static IServiceScopeFactory _serviceScopeFactory;
 
-        public static void Info(string message)
+        public static void Initialize(IServiceScopeFactory serviceScopeFactory)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write($"{Sign}: ");
-            Console.ResetColor();
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm:ss"));
-            Console.WriteLine(message);
+            _serviceScopeFactory = serviceScopeFactory;
         }
 
-        public static void Error(string message)
+        public static void Info<T>(string message)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write($"{Sign}: ");
-            Console.ResetColor();
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm:ss"));
-            Console.WriteLine(message);
-        }
-
-        public static void Exception(Exception ex)
-        {
-            Error(ex.ToString());
-            Exception exception = ex;
-            while (exception.InnerException != null)
+            if (_serviceScopeFactory == null)
             {
-                exception = ex.InnerException;
-                Error(exception.Message);
+                return;
+            }
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                ILogger<T>? service = scope.ServiceProvider.GetService<ILogger<T>>();
+                if (service != null)
+                {
+                    service.LogInformation(message);
+                }
             }
         }
 
+        public static void Error<T>(string message)
+        {
+            if (_serviceScopeFactory == null)
+            {
+                return;
+            }
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                ILogger<T>? service = scope.ServiceProvider.GetService<ILogger<T>>();
+                if (service != null)
+                {
+                    service.LogError(message);
+                }
+            }
+        }
+
+        public static void Exception<T>(Exception ex)
+        {
+            if (_serviceScopeFactory == null)
+            {
+                return;
+            }
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                ILogger<T>? service = scope.ServiceProvider.GetService<ILogger<T>>();
+                if (service != null)
+                {
+                    service.LogError(ex, ex.Message);
+                }
+            }
+        }
 
         public static void PluginBehavior<T>(T plugin, Type iplugin, string methodName)
             where T : IPlugins.IPlugin
         {
+            if (_serviceScopeFactory == null)
+            {
+                return;
+            }
             // TODO: Bug: 无法区别 相同方法名, 参数不同的 重载方法
             MethodInfo behavior = iplugin.GetMethods().FirstOrDefault(m => m.Name == methodName);
 
@@ -70,18 +97,15 @@ namespace PluginCore.Utils
             // A程序集.AClass.AMethod()
             string message = $"{behavior.DeclaringType.ToString()}.{behavior.Name}({parsStr})";
 
-
-            // TODO: 1.写入日志文件
-
-
-            // 2.控制台输出
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write($"{Sign}: {pluginStr}: ");
-            Console.ResetColor();
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm:ss"));
-
-            Console.WriteLine(message);
+            // 2. 日志输出
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                ILogger<T>? service = scope.ServiceProvider.GetService<ILogger<T>>();
+                if (service != null)
+                {
+                    service.LogInformation(message);
+                }
+            }
         }
 
     }
