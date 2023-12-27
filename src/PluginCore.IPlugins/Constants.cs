697fe422408eec364075689c60aa9771113e1bd2
yiyun <yiyungent@gmail.com>
2023-12-27 16:33:50 +0800

    feat(src/plugincore.iplugins/constants.cs): add: AspNetCoreAuthenticationScheme

--- src/PluginCore.IPlugins/Constants.cs
+++ src/PluginCore.IPlugins/Constants.cs
@@ -4,7 +4,9 @@ namespace PluginCore.IPlugins
 {
     public class Constants
     {
-        public const string AspNetCoreAuthenticationClaimType = "PluginCore.Token";
+        public const string AspNetCoreAuthenticationClaimType = "PluginCore.Admin.Token";
+
+        public const string AspNetCoreAuthenticationScheme = "PluginCore.Admin.Authentication";
 
         public const string AspNetCoreAuthorizationPolicyName = "PluginCore.Admin";
 
