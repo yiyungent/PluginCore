a1f83fce8f1dcf2e5ead3b8a15eae006053af4de
yiyun <yiyungent@gmail.com>
2023-12-29 11:09:18 +0800

    fix(src/plugincore.aspnetcore/middlewares/languagemiddleware.cs): namespace: 语法降级

--- src/PluginCore.AspNetCore/Middlewares/LanguageMiddleware.cs
+++ src/PluginCore.AspNetCore/Middlewares/LanguageMiddleware.cs
@@ -13,34 +13,35 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using PluginCore.IPlugins;
 
-namespace PluginCore.AspNetCore.Middlewares;
-
-public class LanguageMiddleware
+namespace PluginCore.AspNetCore.Middlewares
 {
-    private readonly RequestDelegate _next;
-
-    public LanguageMiddleware(RequestDelegate next)
+    public class LanguageMiddleware
     {
-        _next = next;
+        private readonly RequestDelegate _next;
+
+        public LanguageMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // 从 Cookie 中获取语言标识
+            string language = httpContext.Request.Cookies[Constants.AspNetCoreLanguageCookieName];
+
+            // 存储当前语言到 HttpContext.Items
+            httpContext.Items[Constants.AspNetCoreLanguageKey] = language;
+
+            // 调用下一个中间件
+            await _next(httpContext);
+        }
     }
 
-    public async Task Invoke(HttpContext httpContext)
+    public static class LanguageMiddlewareExtensions
     {
-        // 从 Cookie 中获取语言标识
-        string language = httpContext.Request.Cookies[Constants.AspNetCoreLanguageCookieName];
-
-        // 存储当前语言到 HttpContext.Items
-        httpContext.Items[Constants.AspNetCoreLanguageKey] = language;
-
-        // 调用下一个中间件
-        await _next(httpContext);
-    }
-}
-
-public static class LanguageMiddlewareExtensions
-{
-    public static IApplicationBuilder UseLanguageMiddleware(this IApplicationBuilder builder)
-    {
-        return builder.UseMiddleware<LanguageMiddleware>();
+        public static IApplicationBuilder UseLanguageMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LanguageMiddleware>();
+        }
     }
 }
